87ff52efe7aed9c6680d1fd558ea8faa58485cf6
m-e pasta <mepastacc@gmail.com>
2025-11-28 17:28:13 +0100

    implemented eslint

---
+++ src/components/WelcomePage.tsx
@@ -0,0 +1,18 @@
+
+import "../styles/WelcomePageStyle.css";
+
+function App() {
+
+
+  return (
+    <div className="container">
+      <head className="head">
+        <h1 className="title">DB Spyer</h1>
+        <p className="titleDesc">Your complete database visualizer interface</p>
+      </head>
+    </div>
+
+  );
+}
+
+export default App;
